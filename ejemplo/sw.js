ba805bf81deea03ccc970ab6e8a1716a72d29613
ricardocohuo <39710540+ricardocohuo@users.noreply.github.com>
2020-09-24 12:44:39 -0500

    Update sw.js

--- ejemplo/sw.js
+++ ejemplo/sw.js
@@ -2,14 +2,14 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('fox-store').then(function(cache) {
      return cache.addAll([
-       '/PWA1.2/ejemplo/',
-       '/PWA1.2/ejemplo/index.html',
-       '/PWA1.2/ejemplo/index.js',
-       '/PWA1.2/ejemplo/style.css',
-       '/PWA1.2/ejemplo/images/fox1.jpg',
-       '/PWA1.2/ejemplo/images/fox2.jpg',
-       '/PWA1.2/ejemplo/images/fox3.jpg',
-       '/PWA1.2/ejemplo/images/fox4.jpg'
+       '/',
+       '/index.html',
+       '/index.js',
+       '/style.css',
+       '/images/fox1.jpg',
+       '/images/fox2.jpg',
+       '/images/fox3.jpg',
+       '/images/fox4.jpg'
      ]);
    })
  );
